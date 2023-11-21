f3a2037163d79f68d0717bf7e97f15f0d3feed35
Gabriel Resendiz <gabriel.resendizmx@outlook.com>
2023-11-21 12:00:15 -0600

    primer commit

---
+++ App.js
@@ -0,0 +1,84 @@
+import { StyleSheet, Platform, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import Header from './src/componets/Header';
+import Timer from './src/componets/Timer';
+import { Audio } from 'expo-av';
+
+const colors = ['#38CFFF', '#FD4ECD', '#FF2D2D']
+
+export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime ] = useState('POMO' | 'SHORT' | 'BREAK');
+  const [isActive, setIsActive] = useState(false);
+  
+  useEffect(() => {
+    let interval = null;
+
+    if(isActive){
+      //run timer 
+      interval = setInterval(() =>{
+        setTime(time - 1)
+      }, 1000);
+    }else{
+      //clear interval
+      clearInterval(interval);
+    }
+
+    if ( time === 0) {
+      setIsActive(false);
+      setIsWorking((prev) => !prev);
+      setTime(isWorking ? 300 : 1500);
+    }
+
+    return () => clearInterval(interval)
+  }, [isActive, time])
+
+  function handleStartStop (){
+    playSound();
+    setIsActive(!isActive);
+  }
+
+ async function playSound (){
+  const { sound } = await Audio.Sound.createAsync( require("./assets/click.mp3"));
+  await sound.playAsync();
+  }
+
+  return (
+    <SafeAreaView style={[styles.container, {backgroundColor: colors[currentTime] }]}>
+      <View 
+      style={{
+        flex: 1,
+        paddingHorizontal: 15, 
+        paddingTop: Platform.OS ==="android" && 30
+      }}
+      >      
+        <Text style={styles.text}>Time to study</Text>
+        <Header currentTime={currentTime} setCurrentTime={setCurrentTime} setTime={setTime} 
+        />
+        <Timer time={time}/>
+        <TouchableOpacity onPress={handleStartStop} style={styles.button}>
+          <Text style={{color: 'white', fontWeight: 'bold'}}>{isActive ? 'STOP' : 'START'}</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 32, 
+    fontWeight: "bold",
+    
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#333333',
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+  }
+});
